0e7f11ef8808e40b5d041c8cd1ca2f73dd0149b6
Desertt <mustafadagtekin@gmail.com>
2021-11-16 00:46:03 +0300

    ViewComponent

---
+++ core_bookcase/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using core_bookcase.Models;
+
+namespace core_bookcase.ViewComponents
+{
+    public class YeniKitaplar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitapListesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    ID=6,
+                    KitapAd="Korku",
+                    Yazar="Zweig"
+                 },
+
+                new Kitaplar
+                 {
+                    ID=7,
+                    KitapAd="Korku",
+                    Yazar="Zweig"
+
+                }
+            };
+            return View(kitapListesi);
+        }
+
+    }
+
+}
+
